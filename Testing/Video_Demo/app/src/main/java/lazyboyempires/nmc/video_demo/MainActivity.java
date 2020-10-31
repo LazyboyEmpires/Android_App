598d49fcb334dc50d5011b1d8cffdd08848d764a
LazyboyEmpires <62291547+LazyboyEmpires@users.noreply.github.com>
2020-10-31 15:13:46 +0700

    init commit

---
+++ Testing/Video_Demo/app/src/main/java/lazyboyempires/nmc/video_demo/MainActivity.java
@@ -0,0 +1,18 @@
+package lazyboyempires.nmc.video_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        videoView.start();
+    }
+}
